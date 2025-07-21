4aa5f13877896276a69cf966217678fe7ce08576
mohitmaikhuri03 <mkmaikhuri@gmail.com>
2025-07-21 16:34:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,17 +4,20 @@ provider "aws" {
 
 module "aws_inspector_2" {
   source = "./inspector"
-
   aws_inspector_2_enable           = var.aws_inspector_2_enable
   enabled_resources_admin_account  = var.enabled_resources_admin_account
 
-  initialize_delegated_admin_account = false
-  enable_delegated_admin_account     = false
-  enable_member_accounts             = false
-  member_accounts                    = []
-  enabled_resources_member_account   = []
-  auto_enable                        = []
-  auto_enable_member_accounts        = false
-  enable_account_associate           = false
-  account_associate                  = null
+  initialize_delegated_admin_account = var.initialize_delegated_admin_account
+  enable_delegated_admin_account     = var.enable_delegated_admin_account
+
+  enable_member_accounts           = var.enable_member_accounts
+  member_accounts                  = var.member_accounts
+  enabled_resources_member_account = var.enabled_resources_member_account
+
+  auto_enable                 = var.auto_enable
+  auto_enable_member_accounts = var.auto_enable_member_accounts
+
+  enable_account_associate = var.enable_account_associate
+  account_associate        = var.account_associate
 }
+
